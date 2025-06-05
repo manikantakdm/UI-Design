34da258319e49508e549a230e104511211086488
manikantakdm <manikantakdm16@gmail.com>
2025-06-05 11:11:12 +0530

    Update mobile.dart

--- LAB7/mobile.dart
+++ LAB7/mobile.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Mobile extends StatelessWidget {
+  const Mobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Mobile Layout',
+        style: TextStyle(fontSize: 24, color: Colors.blue),
+      ),
+    );
+  }
+}
 
