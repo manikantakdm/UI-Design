816fccd378e75ac48d7448b61b0cd0054d412218
manikantakdm <manikantakdm16@gmail.com>
2025-06-05 11:09:52 +0530

    Update main.dart

--- LAB7/main.dart
+++ LAB7/main.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
+import 'mobile.dart';
+import 'tablet.dart';
+import 'pc.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MediaQuery',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Desktop(),
+    );
+  }
+}
+
+class Desktop extends StatefulWidget {
+  const Desktop({super.key});
+
+  @override
+  State<Desktop> createState() => _Desktop();
+}
+
+class _Desktop extends State<Desktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Responsive UI')),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            return const Mobile();
+          } else if (constraints.maxWidth >= 600 && constraints.maxWidth <= 1200) {
+            return const Tablet();
+          } else {
+            return const PC();
+          }
+        },
+      ),
+    );
+  }
+}
 
