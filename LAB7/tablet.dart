dd365b5c574c2c2c62d242be6ce01158553c0bb2
manikantakdm <manikantakdm16@gmail.com>
2025-06-05 11:12:46 +0530

    Update tablet.dart

--- LAB7/tablet.dart
+++ LAB7/tablet.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Tablet extends StatelessWidget {
+  const Tablet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Tablet Layout',
+        style: TextStyle(fontSize: 28, color: Colors.orange),
+      ),
+    );
+  }
+}
 
