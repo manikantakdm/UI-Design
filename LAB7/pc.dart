eb6a65b88137fd24851c53a9c34e457d7659a6e6
manikantakdm <manikantakdm16@gmail.com>
2025-06-05 11:15:10 +0530

    Update pc.dart

--- LAB7/pc.dart
+++ LAB7/pc.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+
+class PC extends StatelessWidget {
+  const PC({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'PC Layout',
+        style: TextStyle(fontSize: 32, color: Colors.green),
+      ),
+    );
+  }
+}
 
